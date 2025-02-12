80b10a27028b02e6a7a08bf571845a1434dc21c4
Bonmin Ku <bonkri999@gmail.com>
2025-02-12 22:58:28 +0900

    init: git init

---
+++ src/main/java/com/achiko/backend/AchikoBackApplication.java
@@ -0,0 +1,13 @@
+package com.achiko.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchikoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AchikoBackApplication.class, args);
+	}
+
+}
