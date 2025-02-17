365af767649765fb906e1f3a13b8cc7470b3c89f
parksy-13 <psy9905@gmail.com>
2025-02-17 16:59:32 +0900

    feat: ChatRoomEntity에 toEntity 추가

--- backend/src/main/java/com/achiko/backend/entity/ChatRoomEntity.java
+++ backend/src/main/java/com/achiko/backend/entity/ChatRoomEntity.java
@@ -1,5 +1,51 @@
 package com.achiko.backend.entity;
 
-public class ChatRoomEntity {
+import java.time.LocalDateTime;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.achiko.backend.dto.ChatRoomDTO;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+@Entity
+public class ChatRoomEntity {
+	@Id
+	@Column(name="chatroom_id")
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private Integer chatroomId;
+	
+	@ManyToOne
+	@JoinColumn(name="shareId", referencedColumnName="shareId")
+	private ShareEntity share;
+	
+	@Column(name="createdAt")
+	@CreationTimestamp
+	private LocalDateTime createdAt;
+
+	public static ChatRoomEntity toEntity(ChatRoomDTO roomDTO, ShareDTO shareDTO) {
+		return ChatRoomEntity.builder().
+				chatroomId(roomDTO.getChatroomId())
+				.share(shareDTO)
+				.build();
+	}
 }
+ 
