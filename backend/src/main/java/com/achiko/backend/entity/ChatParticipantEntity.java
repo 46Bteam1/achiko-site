581a8fe7b94f5b08102643474afba104c0857c28
parksy-13 <psy9905@gmail.com>
2025-02-17 17:23:42 +0900

    feat: ChatParticipantEntity 수정

--- backend/src/main/java/com/achiko/backend/entity/ChatParticipantEntity.java
+++ backend/src/main/java/com/achiko/backend/entity/ChatParticipantEntity.java
@@ -1,5 +1,34 @@
 package com.achiko.backend.entity;
 
-public class ChatParticipantEntity {
+import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+
+public class ChatParticipantEntity {
+	@Id
+	@Column(name="participant_id")
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private Integer participantId;
+	
+	@OneToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="chatroom_id", referencedColumnName="chatroom_id")
+	private ChatRoomEntity chatroomEntity;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="host_id", referencedColumnName="user_id")
+	private UserEntity host;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="guest_id", referencedColumnName="user_id")
+	private UserEntity guestId;
+	
+	private String role;
+	private LocalDateTime joinedAt;
 }
