c9c8d0c94bafd5fb69771ae166fe9cdccac4dede
SONG JIYOUL <jysong0225@gmail.com>
2025-02-17 15:23:58 +0900

    feat: 회원가입 기능 구현

---
+++ backend/src/main/java/com/achiko/backend/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.achiko.backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.achiko.backend.dto.UserDTO;
+import com.achiko.backend.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+	
+	private final UserService userService;
+	
+	@GetMapping("/regist")
+	public String regist() {
+		return "/user/regist";
+	}
+	
+	@PostMapping("/regist")
+	public String regist(@ModelAttribute UserDTO userDTO) {
+		
+		userService.regist(userDTO);
+		return "redirect:/";
+	}
+	
+}
