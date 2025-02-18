d6fa687d5f9c2b0ea39efb161cc9294abe3b8b2b
parksy-13 <psy9905@gmail.com>
2025-02-18 17:41:22 +0900

    feat: selectRoom, deleteRoom, saveMessage 메서드 생성

--- backend/src/main/java/com/achiko/backend/controller/ChatController.java
+++ backend/src/main/java/com/achiko/backend/controller/ChatController.java
@@ -1,9 +1,11 @@
 package com.achiko.backend.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.achiko.backend.service.ChatService;
 
@@ -18,17 +20,31 @@ public class ChatController {
 	// 채팅방 생성 메서드
 	@PostMapping("/create")
 	public String createRoom() {
-		return "";
+		return "/chat/chatList";
 	}
 	
-	// 채팅방 조회 메서드
+	// 채팅방들 조회 메서드
 	@GetMapping("/selectRooms")
 	public String selectRooms() {
-		return "";
+		return "/chat/chatList";
 	}
 	
+	// 특정 채팅방의 채팅 메세지들 조회 메서드
+	@GetMapping("/selectRoom")
+	public String selectRoom() {
+		return "/chat/chatRoom";
+	}
 	
 	// 채팅방 삭제 메서드
+	@DeleteMapping("/deleteRoom")
+	public String deleteRoom() {
+		return "/chat/chatList";
+	}
 	
 	// 채팅 메세지 작성 메서드
+	@PostMapping("/saveMessage")
+	@ResponseBody
+	public String saveMessage() {
+		return "";
+	}
 }
