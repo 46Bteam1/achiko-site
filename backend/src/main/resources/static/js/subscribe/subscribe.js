415ef62cb8a324c1cf45129f565c4fed6872916c
bonrki9 <bonkri999@gmail.com>
2025-03-09 22:37:42 +0900

    fix: 구독 페이지에 header fragment 적용

--- backend/src/main/resources/static/js/subscribe/subscribe.js
+++ backend/src/main/resources/static/js/subscribe/subscribe.js
@@ -114,4 +114,31 @@ $(document).ready(function () {
       },
     });
   });
+
+  // Fragment가 동적으로 로드된 후 이벤트 바인딩
+  $(document).on("click", "#menuButton", function (event) {
+    event.stopPropagation();
+    const $modalMenu = $("#modalMenu");
+
+    if ($modalMenu.is(":visible")) {
+      $modalMenu.hide();
+    } else {
+      $modalMenu.show();
+    }
+  });
+
+  // 모달 바깥 클릭 시 모달 닫기
+  $(document).on("click", function (event) {
+    if (
+      !$("#modalMenu").is(event.target) &&
+      !$("#modalMenu").has(event.target).length &&
+      !$("#menuButton").is(event.target)
+    ) {
+      $("#modalMenu").hide();
+    }
+  });
+
+  // header관련
+  $("header").addClass("sticky"); // sticky가 add되면 작은 검색창 나옴
+  $("header").removeClass("sticky-reappear");
 });
