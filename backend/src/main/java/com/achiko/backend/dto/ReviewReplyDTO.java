56fcb3b9cdb6c51488bc56b5a1d8f4ebaf6fe41f
Gahee Kang <riverside.hee@gmail.com>
2025-03-09 22:32:15 +0900

    fix: review reply 를 mypage에 보여주기 위한 수정

--- backend/src/main/java/com/achiko/backend/dto/ReviewReplyDTO.java
+++ backend/src/main/java/com/achiko/backend/dto/ReviewReplyDTO.java
@@ -1,41 +1,44 @@
-//package com.achiko.backend.dto;
-//
-//import java.time.LocalDateTime;
-//
-//import com.achiko.backend.entity.FavoriteEntity;
-//import com.achiko.backend.entity.ShareEntity;
-//import com.achiko.backend.entity.UserEntity;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//
-//@Getter
-//@Setter
-//@AllArgsConstructor
-//@NoArgsConstructor
-//public class ReviewReplyDTO {
-//	
-//    private Long replyId;
-//    private Long reviewId;
-//    private Long replyUserId;
-//    private String content;
-//    private LocalDateTime createdAt;
-//    
-//	private String shareTitle;
-//
-//	public static ReviewReplyDTO toDTO(ReviewReplyEntity entity) {
-//    	return ReviewReplyDTO.builder()
-//    			.replyId(dto.getReplyId())
-//				.review(reviewEntity)
-//				.user(userEntity)
-//				.content(dto.getContent())
-//				.createdAt(dto.getCreatedAt())
-//				.build();
-//    }
-//}
-//
+package com.achiko.backend.dto;
+
+import java.time.LocalDateTime;
+
+import com.achiko.backend.entity.FavoriteEntity;
+import com.achiko.backend.entity.ReviewReplyEntity;
+import com.achiko.backend.entity.ShareEntity;
+import com.achiko.backend.entity.UserEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ReviewReplyDTO {
+	
+    private Long replyId;
+    private Long reviewId;
+    private Long replyUserId;
+    private String content;
+    private LocalDateTime createdAt;
+
+	public static ReviewReplyDTO toDTO(ReviewReplyEntity entity) {
+    	return ReviewReplyDTO.builder()
+    			.replyId(entity.getReplyId())
+				.reviewId(entity.getReview().getReviewId())
+				.replyUserId(entity.getUser().getUserId())
+				.content(entity.getContent())
+				.createdAt(entity.getCreatedAt())
+				.build();
+    }
+}
+
 ///*
 // * create table review_reply (
 //    reply_id int auto_increment not null primary key,
--- backend/src/main/java/com/achiko/backend/repository/ReviewReplyRepository.java
+++ backend/src/main/java/com/achiko/backend/repository/ReviewReplyRepository.java
@@ -1,11 +1,11 @@
-//package com.achiko.backend.repository;
-//
-//import java.util.List;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import com.achiko.backend.entity.ReviewReplyEntity;
-//
-//public interface ReviewReplyRepository extends JpaRepository<ReviewReplyEntity, Long> {
-////    List<ReviewReplyEntity> findByUserId(Long userId);
-//}
+package com.achiko.backend.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.achiko.backend.entity.ReviewReplyEntity;
+
+public interface ReviewReplyRepository extends JpaRepository<ReviewReplyEntity, Long> {
+    List<ReviewReplyEntity> findByUser_userId(Long userId);
+}
