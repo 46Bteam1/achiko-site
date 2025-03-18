209690005ea354711134d542df8de730cadbb2f5
Bonmin Ku <bonkri999@gmail.com>
2025-03-18 15:15:24 +0900

    chore: 운영 서버에서 로컬로 변경

--- backend/src/main/resources/static/js/chat/chatRooms.js
+++ backend/src/main/resources/static/js/chat/chatRooms.js
@@ -4,8 +4,8 @@ $(function () {
   const userId = $("#userId").val();
 
   $("#toMyPageBtn").on("click", function () {
-    // window.location.href = `http://localhost:8080/mypage/mypageSample?userId=${userId}`;
-    window.location.href = `https://achiko.site/mypage/mypageSample?userId=${userId}`;
+    window.location.href = `http://localhost:9905/mypage/mypageSample?userId=${userId}`;
+    // window.location.href = `https://achiko.site/mypage/mypageSample?userId=${userId}`;
   });
 });
 
